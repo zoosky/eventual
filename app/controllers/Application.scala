e13331912cd6d3400e11fd8c9ec125ffc55441fc
angyjoe <angyjoe@gmail.com>
2013-11-11 10:30:36 +0100

    Adding the index page and the AngularJS and Play routes to it.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,35 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import java.io.File
+
+import play.Play
+import play.api.mvc.Action
+import play.api.mvc.Controller
+
+/*
+ * Author: Sari Haj Hussein
+ */
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  /** this method serves the index page app/views/index.scala.html */
+  def index(any: String) = Action {
+    Ok(views.html.index())
   }
   
+  /** this method resolves "any" into the corresponding HTML page URI */
+  def getURI(any: String): String = any match {
+    case "main" => "/public/html/main.html"
+    case _ => "error"
+  }
+  
+  /** this method loads an HTML page from public/html */
+  def loadPublicHTML(any: String) = Action {
+    val projectRoot = Play.application().path()
+    var file = new File(projectRoot + getURI(any))
+    if (file.exists())
+      Ok(scala.io.Source.fromFile(file.getCanonicalPath()).mkString).as("text/html");
+    else
+      NotFound
+  }
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,22 @@
 /*
  * Author: Sari Haj Hussein
  */
+var app = angular.module("app", ["ngResource"])
+	.constant("apiUrl", "http://localhost:9000\:9000/api") // to tell AngularJS that 9000 is not a dynamic parameter
+	.config(["$routeProvider", function($routeProvider) {
+		return $routeProvider.when("/", {
+			templateUrl: "/views/main",
+			controller: "AppCtrl"
+		}).otherwise({
+			redirectTo: "/"
+		});
+	}
+	]).config([
+	"$locationProvider", function($locationProvider) {
+		return $locationProvider.html5Mode(true).hashPrefix("!"); // enable the new HTML5 routing and histoty API
+	}
+]);
+
+// global controller
+app.controller("AppCtrl", ["$scope", function($scope) {
+}]);
