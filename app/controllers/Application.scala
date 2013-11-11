c41c8cd033d7dc41b296fa224cc9f35cca43af16
angyjoe <angyjoe@gmail.com>
2013-11-11 18:11:53 +0100

    Adding Name and Celebrity models.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,18 +12,18 @@ import play.api.mvc.Controller
 
 object Application extends Controller {
   
-  /** this method serves the index page app/views/index.scala.html */
+  /** serve the index page app/views/index.scala.html */
   def index(any: String) = Action {
     Ok(views.html.index())
   }
   
-  /** this method resolves "any" into the corresponding HTML page URI */
+  /** resolve "any" into the corresponding HTML page URI */
   def getURI(any: String): String = any match {
     case "main" => "/public/html/main.html"
     case _ => "error"
   }
   
-  /** this method loads an HTML page from public/html */
+  /** load an HTML page from public/html */
   def loadPublicHTML(any: String) = Action {
     val projectRoot = Play.application().path()
     var file = new File(projectRoot + getURI(any))
