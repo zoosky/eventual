40820a23552c2efacab360610b1cd2e3a48c9064
angyjoe <angyjoe@gmail.com>
2014-07-12 19:58:40 +0200

    Adding the Application controller.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,34 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import java.io.File
 
+import play.Play
+import play.api.mvc.Action
+import play.api.mvc.Controller
+
+/*
+ * Author: Sari Haj Hussein
+ */
 object Application extends Controller {
-
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  /** serve the index page app/views/index.scala.html */
+  def index(any: String) = Action {
+    Ok(views.html.index())
   }
 
+  /** resolve "any" into the corresponding HTML page URI */
+  def getURI(any: String): String = any match {
+    case "main" => "/public/html/main.html"
+    case "detail" => "/public/html/detail.html"
+    case _ => "error"
+  }
+
+  /** load an HTML page from public/html */
+  def loadPublicHTML(any: String) = Action {
+    val projectRoot = Play.application().path()
+    val file = new File(projectRoot + getURI(any))
+    if (file.exists())
+      Ok(scala.io.Source.fromFile(file.getCanonicalPath()).mkString).as("text/html");
+    else
+      NotFound
+  }
 }
